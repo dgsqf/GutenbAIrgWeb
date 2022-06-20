71b3ab0d8d970bd38053709d17e8e0983e823932
dgsqef <etiennebounias@orange.fr>
2022-06-20 14:21:11 +0200

    start of real dev

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,8 @@
 <script setup>
 
 </script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
 
 <template>
-  <h1 class="object-center">Hello World</h1>
+  <h1 class="label is-primary">Hello World</h1>
 </template>
-
-<style>
-@import './assets/base.css';
-</style>
